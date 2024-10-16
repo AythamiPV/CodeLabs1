fcf2dcd1c8ff155cd65047c2c712fe3276069321
DESKTOP-VK5GCUR\Aythami Pérez <aypeve@gmail.com>
2024-10-16 12:15:21 +0100

    Seconds steps

--- src/Main.kt
+++ src/Main.kt
@@ -5,6 +5,23 @@ fun main() {
     // to see how IntelliJ IDEA suggests fixing it.
     println("Hello, world!")
 
-    val count: Int = 0
-    println(count)
+    var count: Int = 0
+    println("You have unread $count messages")
+
+
+    count++//Increase by 1.
+    println("You have unread $count messages")
+
+    count-- //Decrease by 1.
+    readMessage(showMessage(), count)
+
+}
+
+fun showMessage(): String {
+    val message: String = "Hello developer!"
+    return message
+}
+fun readMessage(message: String, countMessage: Int){
+    println(message)
+    println("You have unread $countMessage messages")
 }
